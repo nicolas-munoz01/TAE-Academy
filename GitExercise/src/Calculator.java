3ce3d356c0f68547e0be3b1e3f35d48126945643
nicolas-munoz01 <98959106+nicolas-munoz01@users.noreply.github.com>
2022-02-06 19:04:40 -0500

    Adding mutltiplication and division methods

--- GitExercise/src/Calculator.java
+++ GitExercise/src/Calculator.java
@@ -1,2 +1,10 @@
 public class Calculator {
+
+    public double multiplicationOperation(double firstNumber, double secondNumber){
+        return  firstNumber * secondNumber;
+    }
+
+    public double divisionOperation(double firstNumber, double secondNumber){
+        return  firstNumber / secondNumber;
+    }
 }
